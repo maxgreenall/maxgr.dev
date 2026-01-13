5451614ce765a123f8e4195a122df9a50afff7fc
Max Greenall <maxgreenall@gmail.com>
2026-01-13 14:21:13 +0000

    feat: project update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,58 +1,85 @@
 import { Button, Tooltip } from "@nextui-org/react";
 import { useState } from "react";
-import confetti from 'canvas-confetti';
+import confetti from "canvas-confetti";
 
 function Hero({ onContactPress }: { onContactPress: () => void }) {
-    const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-    const incrementCounter = () => {
-        setCounter(counter + 1);
-        handleConfetti();
-    };
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+    handleConfetti();
+  };
 
-    function randomInRange(min:number, max:number) {
-        return Math.random() * (max - min) + min;
+  function randomInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const handleConfetti = () => {
+    const button = document.getElementById("confetti-button");
+
+    if (button) {
+      const buttonRect = button.getBoundingClientRect();
+      const buttonX = buttonRect.left + buttonRect.width / 2;
+      const buttonY = buttonRect.top + buttonRect.height / 2;
+
+      const scalar = 2;
+      const shapes = confetti.shapeFromText({ text: "❤️", scalar });
+
+      confetti({
+        angle: randomInRange(45, 135),
+        spread: 360,
+        particleCount: randomInRange(20, 50),
+        shapes: [shapes],
+        scalar: scalar,
+        origin: {
+          x: buttonX / window.innerWidth,
+          y: buttonY / window.innerHeight,
+        },
+      });
     }
-    
-    const handleConfetti = () => {
-        const button = document.getElementById('confetti-button');
+  };
 
-        if (button) {
-            const buttonRect = button.getBoundingClientRect();
-            const buttonX = buttonRect.left + (buttonRect.width / 2);
-            const buttonY = buttonRect.top + (buttonRect.height / 2);
+  return (
+    <div className="h-[calc(100vh-64px)] flex flex-col justify-center items-center gap-6">
+      <h1 className="font-bold text-[2.5rem] lg:text-5xl text-center ">
+        Hi I'm{" "}
+        <span className="bg-gradient-to-r from-purple-500 to-blue-500 text-transparent bg-clip-text">
+          Max
+        </span>
+      </h1>
+      <h2 className="text-center text-xl max-w-xl text-gray-400">
+        I'm a full stack engineer, building, hacking and breaking things.
+      </h2>
 
-            const scalar = 2;
-            const shapes = confetti.shapeFromText({ text: '❤️', scalar });
-
-            confetti({
-                angle: randomInRange(45, 135),
-                spread: 360,
-                particleCount: randomInRange(20, 50),
-                shapes: [shapes],
-                scalar: scalar,
-                origin: { x: buttonX / window.innerWidth, y: buttonY / window.innerHeight }
-            });
-        }
-    };
-
-    return(
-        <div className="h-[calc(100vh-64px)] flex flex-col justify-center items-center gap-6">
-            <h1 className="font-bold text-[2.5rem] lg:text-5xl text-center ">
-                Hi I'm <span className="bg-gradient-to-r from-purple-500 to-blue-500 text-transparent bg-clip-text">Max</span>
-            </h1>
-            <h2 className="text-center text-xl max-w-xl text-gray-400">I'm a full stack web developer specializing in Blazor, proficient in both front and back-end development.</h2>
-
-            <div className="flex gap-3 justify-center">
-                <Button size="lg" color="primary" variant="shadow" onClick={onContactPress}>Contact</Button>
-                <Tooltip showArrow={true} color="danger" content={`Counter: ${counter}`}>
-                    <Button size="lg" isIconOnly aria-label="Like" color="danger" variant="flat" onClick={incrementCounter} id="confetti-button">
-                        ❤️
-                    </Button>
-                </Tooltip>
-            </div>
-        </div>
-    )
+      <div className="flex gap-3 justify-center">
+        <Button
+          size="lg"
+          color="primary"
+          variant="shadow"
+          onClick={onContactPress}
+        >
+          Contact
+        </Button>
+        <Tooltip
+          showArrow={true}
+          color="danger"
+          content={`Counter: ${counter}`}
+        >
+          <Button
+            size="lg"
+            isIconOnly
+            aria-label="Like"
+            color="danger"
+            variant="flat"
+            onClick={incrementCounter}
+            id="confetti-button"
+          >
+            ❤️
+          </Button>
+        </Tooltip>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,30 +1,67 @@
 //import { Button } from "@nextui-org/react";
-import IframeLogo from '../assets/iframequicklink-logo.png';
-import WhiteboardLogo from '../assets/whiteboard-logo.png';
-import QueryLogo from '../assets/queryparameter-logo.png';
+import IframeLogo from "../assets/iframequicklink-logo.png";
+import WhiteboardLogo from "../assets/whiteboard-logo.png";
+import QueryLogo from "../assets/queryparameter-logo.png";
+import BeaconLogo from "../assets/beacon-logo.png";
+import AOSyncLogo from "../assets/aosync-logo.svg";
 import LinkCard from "./linkcard";
 
-function Projects(){
-    return (
-        <div className="py-28">
-            <div className="pb-10">
-                <h1 className="font-bold text-3xl text-center pb-2">My projects</h1>
-                <h1 className="text-xl text-center text-gray-400">See what I've been working on 👀</h1>
-            </div>
+function Projects() {
+  return (
+    <div className="py-28">
+      <div className="pb-10">
+        <h1 className="font-bold text-3xl text-center pb-2">My projects</h1>
+        <h1 className="text-xl text-center text-gray-400">
+          See what I've been working on 👀
+        </h1>
+      </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <LinkCard imageUrl={IframeLogo} title={"Iframe Quicklink"} subtitle={"Chrome extension"} link={"https://chromewebstore.google.com/detail/iframe-quicklink-open-ifr/nfjhbjdckjldofelpokknfmbaaflfobm"} ></LinkCard>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <LinkCard
+          imageUrl={BeaconLogo}
+          title={"Beacon DeFi"}
+          subtitle={"iOS app"}
+          link={"https://getbeaconapp.com/"}
+        ></LinkCard>
 
-                <LinkCard imageUrl={WhiteboardLogo} title={"Whiteboards"} subtitle={"Trello powerup"} link={"https://trello.com/power-ups/657d870cac9ff5f8af13b518/"} ></LinkCard>
+        <LinkCard
+          imageUrl={AOSyncLogo}
+          title={"AO Sync"}
+          subtitle={"React SDK"}
+          link={"https://github.com/vela-ventures/ao-sync-sdk"}
+        ></LinkCard>
 
-                <LinkCard imageUrl={QueryLogo} title={"Query Parameter Editor"} subtitle={"Chrome extension"} link={"https://chromewebstore.google.com/detail/query-parameter-editor/kpdhkjadlkaflemjhhbfafmjaapfbfjb"} ></LinkCard>
-            </div>
+        <LinkCard
+          imageUrl={IframeLogo}
+          title={"Iframe Quicklink"}
+          subtitle={"Chrome extension"}
+          link={
+            "https://chromewebstore.google.com/detail/iframe-quicklink-open-ifr/nfjhbjdckjldofelpokknfmbaaflfobm"
+          }
+        ></LinkCard>
 
-            {/* <div className="pt-5 flex justify-center">
+        <LinkCard
+          imageUrl={WhiteboardLogo}
+          title={"Whiteboards"}
+          subtitle={"Trello powerup"}
+          link={"https://trello.com/power-ups/657d870cac9ff5f8af13b518/"}
+        ></LinkCard>
+
+        <LinkCard
+          imageUrl={QueryLogo}
+          title={"Query Parameter Editor"}
+          subtitle={"Chrome extension"}
+          link={
+            "https://chromewebstore.google.com/detail/query-parameter-editor/kpdhkjadlkaflemjhhbfafmjaapfbfjb"
+          }
+        ></LinkCard>
+      </div>
+
+      {/* <div className="pt-5 flex justify-center">
                 <Button color="primary" variant="flat">See all</Button>
             </div> */}
-        </div>
-    );
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
